fd1a22b19b5eaff2f30bea6282334de864679deb
Martin M <mudden@live.se>
2024-01-27 10:36:54 +0100

    Update main.js
    Added function captureRoute to handle URL errors when refreshing.

--- src/main.js
+++ src/main.js
@@ -15,12 +15,21 @@ const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Home },
-    { path: '/About', component: About },
-    { path: '/Projects', component: Projects },
-    { path: '/Contact', component: Contact }
+    { path: '/', component: Home, beforeEnter: captureRoute },
+    { path: '/About', component: About, beforeEnter: captureRoute },
+    { path: '/Projects', component: Projects, beforeEnter: captureRoute },
+    { path: '/Contact', component: Contact, beforeEnter: captureRoute }
   ]
 })
+
+function captureRoute(to, from, next) {
+  const newPath = to.path
+  const oldPath = from.path
+
+  next()
+
+}
+router.beforeEach(captureRoute)
 
 app.use(router)
 app.mount('#app')
